7ad36c332aeb8045d64f51f5fae42e3e7e47cf67
адина <adina.atakulova04@gmail.om>
2023-04-12 15:44:48 +0300

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,55 @@
+public class Cat {
+    private String name;
+    private String color;
+    private int age;
+    private int teeth;
+
+
+    public Cat(String name, String color, int age, int teeth) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.teeth = teeth;
+    }
+
+    public Cat(){
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTeeth() {
+        return teeth;
+    }
+
+    public void setTeeth(int teeth) {
+        this.teeth = teeth;
+    }
+
+  //  private  Cat(){
+
+
+
+}
+
---
+++ src/Fish.java
@@ -0,0 +1,53 @@
+public class Fish {
+    private String name;
+    private String color;
+    private int age;
+    private int teeth;
+
+    public Fish(){
+
+    }
+
+    public Fish(String name, String color, int age, int teeth) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.teeth = teeth;
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTeeth() {
+        return teeth;
+    }
+
+    public void setTeeth(int teeth) {
+        this.teeth = teeth;
+    }
+}
+
+
